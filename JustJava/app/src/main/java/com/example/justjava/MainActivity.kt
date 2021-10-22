eda73765f8bab6a215b451fa7bad26ae575fbac3
leoB <leonardozandrade@gmail.com>
2021-10-21 22:57:14 -0300

    Associando eventos

--- JustJava/app/src/main/java/com/example/justjava/MainActivity.kt
+++ JustJava/app/src/main/java/com/example/justjava/MainActivity.kt
@@ -2,27 +2,32 @@ package com.example.justjava
 
 import android.os.Bundle
 import android.view.View
-import androidx.annotation.NonNull
 import androidx.appcompat.app.AppCompatActivity
 import com.example.justjava.databinding.ActivityMainBinding
+import java.text.NumberFormat
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = ActivityMainBinding.inflate(layoutInflater);
         setContentView(binding.root);
         binding.button.setOnClickListener(submitOrder(binding));
+        binding.price.text = NumberFormat.getCurrencyInstance().format(0.00);
 
     }
 
     fun submitOrder(binding: ActivityMainBinding): View.OnClickListener? {
         return View.OnClickListener {
             binding.quantityEditText.text = "5";
+            displayPrice(binding);
         };
     };
 
-
-
+    fun displayPrice(binding: ActivityMainBinding) {
+        binding.price.text = NumberFormat.getCurrencyInstance()
+            .format(Integer.parseInt(binding.quantityEditText.text.toString()));
+    }
 }
 
 
